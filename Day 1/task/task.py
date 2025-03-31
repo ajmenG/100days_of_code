0fa1a3a8f436aa8bff48619e04d707cd8dfbc871
ajmenG <aiman1ghanim@gmail.com>
2025-04-01 01:08:42 +0200

    Day 1

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,1 +1,4 @@
-# Write your code below this line 👇
+print("Welcome to the Band name generator!")
+user_name = input("What's your name? ")
+pet_name = input("What's your favorite pet's name? ")
+print("Your Band name could be " + user_name + " " + pet_name)
